294f989e134d72a74ee4e4690686a1f4ad12d52c
Dmitrii Zvezdov <zvezdov7@mail.ru>
2017-10-13 16:27:58 +0300

    add security user for plain access

---
+++ src/main/java/ru/zvezdov/SecurityConfig.java
@@ -0,0 +1,18 @@
+package ru.zvezdov;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Dmitry on 13.10.2017.
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("zvezdov").password("zvezdov").roles("USER");
+    }
+}
